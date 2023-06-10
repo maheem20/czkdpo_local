73cc9319f25f00ea35e53dd1862093f51c48ccd2
maheem_20 <mwistanbuly@gmail.com>
2023-06-10 23:54:37 +0200

    configure CartService to get shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -26,4 +26,7 @@ export class CartService {
     return this.items;
   }
   /* . . . */
+  getShippingPrices() {
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
+  }
 }
