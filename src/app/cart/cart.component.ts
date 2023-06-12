092e810d41c4e4b6f9721089be39f928fd06593a
maheem_20 <mwistanbuly@gmail.com>
2023-06-12 23:04:54 +0200

    define onSubmit() method to process form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -22,4 +22,10 @@ export class CartComponent {
     private formBuilder: FormBuilder
   ) { }
 
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
